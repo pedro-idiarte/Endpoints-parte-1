273a70d26272458627c769889fec763dadcf01a5
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-10-03 13:48:23 -0300

    exercicio endpoints pt1 resolvido

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { usuariosRoutes } from "./routes/usuarios";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -6,9 +7,18 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/", usuariosRoutes); 
+app.use("/", usuariosRoutes); 
 
 app.listen(port, () => {
   console.log(`Esse servidor está rodando em ${port}`);
 });
+
+export { app };
+export default app;
+
+
+
+
+
+
 
--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import db from "../client/db";
 
 const router = Router();
+export const usuariosRoutes = Router();
 
 // Exercicio de CRUD
 // Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
@@ -9,9 +10,51 @@ const router = Router();
 /*
     O recurso usuario deve ter as seguintes propriedades com seus respectivos tipos:
     {
+    
         name: String,
         email: String,
         password: String
     }
+
 */
+
+
+const create = (req, res) => {
+    const user = req.body;
+    const newUser = db.create(user);
+    res.json(newUser);
+};
+
+const findAllById = (req, res) => {
+    const users = db.findAll();
+    res.json(users);
+}
+
+const findOneById = (req, res) => {
+    const id = Number(req.params.id);
+    const user = db.findById(id);
+    res.json(user);
+}
+
+const updateOneById = (req, res) => {
+    const id = Number(req.params.id);
+    const user = req.body;
+    const updatedUser = db.updateById(id, user);
+    res.json(updatedUser);
+}
+
+const removeById = (req, res) => {
+    const id = Number(req.params.id);
+    db.remove(id);
+    res.json({ message: "removido com sucesso!" });
+}
+
+usuariosRoutes.post("/", create);
+usuariosRoutes.get("/", findAllById);
+usuariosRoutes.get("/:id", findOneById);
+usuariosRoutes.put("/:id", updateOneById);
+usuariosRoutes.delete("/:id", removeById);
+
+      
+export default router;
 
